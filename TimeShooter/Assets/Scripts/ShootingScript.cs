0a75d62ade01924035ab5eeb57dedcf5d9ba60da
DevenSmith <Deven.smith@students.aie.edu.au>
2014-01-02 17:51:19 -0800

    Added TimeShooter Unity

---
+++ TimeShooter/Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum Age {older, younger};
+
+
+public class ShootingScript : MonoBehaviour {
+
+	public string[] ageableObjectTags;
+	public float shootRange;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonDown(0))
+			CheckObject(Age.older);
+		if(Input.GetMouseButtonDown(1))
+			CheckObject(Age.younger);
+
+	
+	}
+
+	void CheckObject(Age effect)
+	{
+		if(effect == Age.older)
+			Debug.Log("Older");
+		else
+			Debug.Log("Younger");
+
+		RaycastHit rayHit;
+		bool hit = Physics.Raycast(transform.position, transform.forward, out rayHit, shootRange);
+		if(hit)
+		{
+			foreach(string tag in ageableObjectTags)
+			{
+				if(rayHit.collider.tag == tag)
+				{
+					if(effect == Age.older)
+						rayHit.transform.gameObject.SendMessage("Aged", 1);
+					else
+						rayHit.transform.gameObject.SendMessage("Aged", -1);
+
+					break;
+
+				}
+			}
+		}
+	}
+
+
+}
---
+++ TimeShooter/Assets/Scripts/TreeScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum Ages {baby, young, normal, old, superOld};
+
+public class TreeScript : MonoBehaviour 
+{
+	Ages age = Ages.normal;
+
+	void Start()
+	{
+		age = (Ages) Random.Range(0,5);
+		Aged(0);
+	}
+
+	void Aged (int effect)
+	{
+		if(effect > 0)
+		{
+			if(age == Ages.superOld)
+				Destroy (this.gameObject);
+			else
+				age++;
+		}
+		else if (effect < 0)
+		{
+			if(age == Ages.baby)
+				Destroy (this.gameObject);
+			else
+				age--;
+		}
+
+		switch(age)
+		{
+		case Ages.baby:
+			transform.localScale = new Vector3(.25f,.5f,.25f);
+			break;
+		case Ages.young:
+			transform.localScale = new Vector3(.25f,3,.25f);
+			break;
+		case Ages.normal:
+			transform.localScale = new Vector3(.75f, 7f, .75f);
+			break;
+		case Ages.old:
+			transform.localScale = new Vector3(2f,12f,2f);
+			break;
+		case Ages.superOld:
+			transform.localScale = new Vector3(3f,24f,3f);
+			break;
+		default:
+			transform.localScale = new Vector3(.5f,1,.5f);
+			break;
+		}
+	}
+}
